979a2c76ce94151acce5c606db0cf7f7665ff356
Rizal <rizalnug2@gmail.com>
2024-12-25 14:54:06 +0700

    perulangan dlm ejs

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/t/:tag', (req, res) => {
-    const {tag} = req.params;
-    res.render('tag', {tag});
-})
+app.get("/t/:tag", (req, res) => {
+  const { tag } = req.params;
+  res.render("tag", { tag });
+});
 
-app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', {num});
-})
+app.get("/cats", (req, res) => {
+  const cats = ["molly", "kitty", "fluffy", "whiskers"];
+  res.render("cats", { cats });
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { num });
+});
 
 app.listen(8080, () => {
-    console.log('Server running on http://localhost:8080');
-})
+  console.log("Server running on http://localhost:8080");
+});
