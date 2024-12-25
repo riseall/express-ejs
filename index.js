43ae095f0a0f307d37747c82896634d86b3379ae
Rizal <rizalnug2@gmail.com>
2024-12-25 16:23:19 +0700

    menampilkan data dari query ke template

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+const tagsData = require("./data.json");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -11,7 +13,12 @@ app.get("/", (req, res) => {
 
 app.get("/t/:tag", (req, res) => {
   const { tag } = req.params;
-  res.render("tag", { tag });
+  const data = tagsData[tag];
+  if (data) {
+    res.render("tag", { data });
+  } else {
+    res.render("notfound", { tag });
+  }
 });
 
 app.get("/cats", (req, res) => {
